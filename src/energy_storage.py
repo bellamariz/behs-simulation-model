6e552328cf8185251a77777ee39428debfa0325f
@bellamariz <isabellafmariz@gmail.com>
2025-06-12 00:28:57 -0300

    feat: Add status attribute to EnergyStorage and update methods for calculating voltage

--- src/energy_storage.py
+++ src/energy_storage.py
@@ -8,21 +8,28 @@ class EnergyStorage:  # considering a simple RC circuit
 
     def __init__(self, Vmax, Vin):
         self.max_voltage = Vmax
-        self.voltage = self.calculate_charging_voltage(0, Vin)
+        self.status = "empty"
+        self.voltage = self.calculate_voltage(0, Vin)
         self.current = self.calculate_current(0, Vin)
         self.charge = self.calculate_charge(self.voltage)
         self.energy = self.calculate_charge(self.voltage)
 
     # Voltage across capacitor when charging, Vc(t), at instant t and given Vin (supply voltage)
     def calculate_charging_voltage(self, t, Vin):
-        if self.is_fully_charged:
-            return self.max_voltage
-
         return Vin * (1 - math.exp(-t/self.TIME_CONSTANT))
 
     # Voltage across capacitor when discharging, Vc(t), at instant t and given Vci (capacitor initial voltage)
     def calculate_discharging_voltage(self, t, Vci):
         return Vci * math.exp(-t/self.TIME_CONSTANT)
+
+    # Voltage across capacitor, Vc(t) at instant t
+    def calculate_voltage(self, t, Vin):
+        if (self.status == "charging") or (self.status == "empty"):
+            return self.calculate_charging_voltage(t, Vin)
+        elif self.status == "discharging":
+            return self.calculate_discharging_voltage(t, self.voltage)
+        elif self.status == "full":
+            return self.max_voltage
 
     # Current flowing through the capacitor; V is Vin (charging) or Vci (discharging)
     def calculate_current(self, t, V):
@@ -41,8 +48,19 @@ class EnergyStorage:  # considering a simple RC circuit
         return self.voltage >= self.max_voltage
 
     # Given an instant t and supply voltage Vin, recalculate the stats for the Energy Storage
-    def refresh_energy_storage(self, t, Vin):
-        self.voltage = self.calculate_charging_voltage(t, Vin)
+    def refresh_energy_storage(self, t, Vin, status):
+        self.status = status
+        self.voltage = self.calculate_voltage(t, Vin)
         self.current = self.calculate_current(t, Vin)
         self.charge = self.calculate_charge(self.voltage)
         self.energy = self.calculate_energy(self.voltage)
+
+    def print(self, t):
+        print(
+            f"t={t},"
+            f"status={self.status},"
+            f"voltage={self.voltage:.5f}V,"
+            f"current={self.current:.5f}A,"
+            f"charge={self.charge:.5f}C,"
+            f"energy={self.energy:.5f}J\n"
+        )
