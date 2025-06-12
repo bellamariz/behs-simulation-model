5a13cec42a620f1ca535433b1015c64fb06131c4
@bellamariz <isabellafmariz@gmail.com>
2025-06-11 23:43:51 -0300

    feat: Add methods for calculating energy storage electric values

--- src/energy_storage.py
+++ src/energy_storage.py
@@ -1,26 +1,48 @@
-class EnergyStorage:
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.current_charge = 0
+import math
 
-    def get_energy_level(self):
-        return self.current_charge
 
-    def get_capacity(self):
-        return self.capacity
+class EnergyStorage:  # considering a simple RC circuit
+    CAPACITANCE = 0.01
+    RESISTANCE = 2500
+    TIME_CONSTANT = CAPACITANCE * RESISTANCE
 
-    def charge(self, amount):
-        if amount < 0:
-            raise ValueError("Amount of energy to store must be non-negative.")
-        self.current_charge += amount
-        if self.current_charge > self.capacity:
-            self.current_charge = self.capacity
+    def __init__(self, Vmax, Vin):
+        self.max_voltage = Vmax
+        self.voltage = self.calculate_charging_voltage(0, Vin)
+        self.current = self.calculate_current(0, Vin)
+        self.charge = self.calculate_charge(self.voltage)
+        self.energy = self.calculate_charge(self.voltage)
 
-    def discharge(self, amount):
-        if amount < 0:
-            raise ValueError(
-                "Amount of energy to retrieve must be non-negative.")
-        if amount > self.current_charge:
-            amount = self.current_charge
-        self.current_charge -= amount
-        return amount
+    # Voltage across capacitor when charging, Vc(t), at instant t and given Vin (supply voltage)
+    def calculate_charging_voltage(self, t, Vin):
+        if self.is_fully_charged:
+            return self.max_voltage
+
+        return Vin * (1 - math.exp(-t/self.TIME_CONSTANT))
+
+    # Voltage across capacitor when discharging, Vc(t), at instant t and given Vci (capacitor initial voltage)
+    def calculate_discharging_voltage(self, t, Vci):
+        return Vci * math.exp(-t/self.TIME_CONSTANT)
+
+    # Current flowing through the capacitor; V is Vin (charging) or Vci (discharging)
+    def calculate_current(self, t, V):
+        return (V / self.RESISTANCE) * math.exp(-t/self.TIME_CONSTANT)
+
+    # Charge flowing through the capacitor, Q(t), at instant t and given Vc(t)
+    def calculate_charge(self, Vc):
+        return self.CAPACITANCE * Vc
+
+    # Energy stored in capacitor, E(t), at instant t and given Vc(t)
+    def calculate_energy(self, Vc):
+        return (1/2) * self.CAPACITANCE * (Vc*Vc)
+
+    # Checks if capacitor is fully charged, i.e. Vc(t) reaches the maximum possible voltage
+    def is_fully_charged(self):
+        return self.voltage >= self.max_voltage
+
+    # Given an instant t and supply voltage Vin, recalculate the stats for the Energy Storage
+    def refresh_energy_storage(self, t, Vin):
+        self.voltage = self.calculate_charging_voltage(t, Vin)
+        self.current = self.calculate_current(t, Vin)
+        self.charge = self.calculate_charge(self.voltage)
+        self.energy = self.calculate_energy(self.voltage)
