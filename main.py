b11b864f55c90f2eefa7b6fe2f7ef67a9a56357a
@bellamariz <isabellafmariz@gmail.com>
2025-06-16 22:16:39 -0300

    feat: Create EnergySupply class

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
 import time
 from src.energy_storage import EnergyStorage
+from src.energy_supply import EnergySupply
+
+
+def generate_t_vector():
+    start = 0.0
+    end = 6.0
+    interval = 0.25
+
+    t_vector = [start + i *
+                interval for i in range(int((end - start) / interval) + 1)]
+
+    return t_vector
 
 
 def main():
-    # create energy storage of maximum voltage Vmax of 300V
-    # considering an initial voltage supply Vin of 10V
-    storage = EnergyStorage(vmax=300, vin=10)
-
-    t_vector = [0.25, 0.5, 0.750, 1, 1.25, 1.5, 1.75, 2.0, 2.25, 2.5, 2.75,
-                3, 3.25, 3.5, 3.75, 4.0, 4.25, 4.5, 4.75, 5, 5.25, 5.5, 5.75, 6]
-    status_vector = ["charging"] * 25
-
-    storage.print(0)
+    t_vector = generate_t_vector()
+    status_vector = EnergyStorage.generate_status_vector(t_vector)
+    v_supply = EnergySupply("energy_neutral", t_vector)
+    storage = EnergyStorage(vmax=10, vin=v_supply.profile[0])
 
     for i, t in enumerate(t_vector):
-        storage.refresh_energy_storage(
-            t=t, vin=10, status=status_vector[i]
+        # storage.refresh_energy_storage(
+        #     t=t, vin=v_supply_vector[i], status=status_vector[i]
+        # )
+        # storage.print(t)
+
+        print(
+            f"t={t},"
+            f"v_supply={v_supply.profile[i]},"
+            f"storage_status={status_vector[i]},"
         )
-        storage.print(t)
         time.sleep(1)
 
 
