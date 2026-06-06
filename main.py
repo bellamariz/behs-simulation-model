2677368abd98692947da3e305a4b7b5f1070b646
@bellamariz <isabellafmariz@gmail.com>
2026-06-05 21:14:26 -0300

    refactor: Separates simulation execution from output functions

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import src.simulator.simulator as simulator
+import src.output.output as out
 
 
 def main():
-    simulator.run()
+    # Run simulation
+    sim_output = simulator.run()
+
+    # Write output to local log file, 'output.log'
+    out.write_to_log(sim_output)
+
+    # Write output to local CSV file, 'output.csv'
+    out.write_to_csv(sim_output)
+
+    # Formats CSV and writes output to local Excel file, 'output.xlsx'
+    out.write_to_excel()
+
+    # Reads Excel file and plots the output
+    out.plot()
 
 
 if __name__ == "__main__":
--- src/simulator/simulator.py
+++ src/simulator/simulator.py
@@ -21,16 +21,34 @@ def run():
     }
     sim_input = inp.Input(input_config, t_vector)
 
-    # Run simulation and write output to local log file, 'output.log'
-    out.write_to_log(t_vector, sim_input.supply,
-                     sim_input.storage, sim_input.load)
+    # Executes the simulation, updating the supply, storage and load at each time t
+    sim_output = {}
 
-    # Run simulation and write output to local CSV file, 'output.csv'
-    out.write_to_csv(t_vector, sim_input.supply,
-                     sim_input.storage, sim_input.load)
+    for i, t in enumerate(t_vector):
+        sim_input.supply.refresh(t_index=i)
+        sim_input.storage.refresh(t_time=t, v_supply=sim_input.supply.voltage,
+                                  load_energy_consumed=sim_input.load.energy_consumed)
+        sim_input.load.refresh(v_supply=sim_input.storage.voltage,
+                               v_load_min=sim_input.storage.V_LOAD_MIN)
 
-    # Formats CSV and writes output to local Excel file, 'output.xlsx'
-    out.write_to_excel()
+        sim_output[t] = {
+            "supply": {
+                "type": sim_input.supply.type,
+                "voltage": sim_input.supply.voltage,
+            },
+            "storage": {
+                "type": sim_input.storage.type,
+                "voltage": sim_input.storage.voltage,
+                "current": sim_input.storage.current,
+                "energy_stored": sim_input.storage.energy_stored,
+            },
+            "load": {
+                "type": sim_input.load.type,
+                "voltage": sim_input.load.voltage,
+                "current": sim_input.load.current,
+                "energy_consumed": sim_input.load.energy_consumed,
+                "total_energy_consumed": sim_input.load.total_energy_consumed,
+            }
+        }
 
-    # Reads Excel file and plots the output
-    out.plot()
+    return sim_output
