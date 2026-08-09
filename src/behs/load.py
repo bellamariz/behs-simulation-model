d3515a1ff06afda26f0224e574420e5b8bdc5e1c
@bellamariz <isabellafmariz@gmail.com>
2026-08-08 21:10:32 -0300

    fix: For the MSP430FR599x MCU, LPM 2.5 preserves SRAM (only reset for shutdown)

--- src/behs/load.py
+++ src/behs/load.py
@@ -174,7 +174,7 @@ class MCU(Load):
             self.mode, self._program_cost = self.program.interface.program_manage_execution(
                 v_supply, t_step, self.program, self.mode, mode_from_supply)
 
-            if self.mode != "active":
+            if self.mode not in ["active", "standby"]:
                 self.program.reset()
         else:
             self.mode = mode_from_supply
